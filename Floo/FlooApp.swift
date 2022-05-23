d2b7a2336e57fd8542136d27b62a665855e67820
Kenny Jinhiro <kjinhiro@student.ciputra.ac.id>
2022-05-23 10:56:10 +0700

    Initial Commit

---
+++ Floo/FlooApp.swift
@@ -0,0 +1,17 @@
+//
+//  FlooApp.swift
+//  Floo
+//
+//  Created by Kenny Jinhiro_0706012010024 on 23/05/22.
+//
+
+import SwiftUI
+
+@main
+struct FlooApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
