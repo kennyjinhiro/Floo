37202e862bbed08cc630596757162edf3f5ff0b3
Kenny Jinhiro <kjinhiro@student.ciputra.ac.id>
2022-06-05 16:14:06 +0700

    best bugfixer on planet

--- Floo/ViewModel/DetailViewModel.swift
+++ Floo/ViewModel/DetailViewModel.swift
@@ -10,7 +10,7 @@ class DetailViewModel: ObservableObject{
     
     @Published var detailedRecipe: DetailedRecipe?
     @Published var nutrients: [Flavonoid] = []
-    @Published var recipe_id = [Int]()
+    @Published var recipe_id: [Int] = []
     
     func fetch_detail(id: Int){
         guard let url = URL(string: "https://api.spoonacular.com/recipes/\(id)/information?apiKey=\(ApiKey.apiKey)&includeNutrition=true")else{
@@ -48,9 +48,11 @@ class DetailViewModel: ObservableObject{
         } else {
             recipe_id = []
         }
-        
+        print("Before Add:", recipe_id)
+        recipe_id = Array(Set(recipe_id))
         recipe_id.append(id)
-        
+        recipe_id = Array(Set(recipe_id))
+        print("After Add:", recipe_id)
         let encoder = JSONEncoder()
         if let encodedData = try? encoder.encode(recipe_id) {
             UserDefaults.standard.set(encodedData, forKey: "recipe_id")
@@ -70,7 +72,9 @@ class DetailViewModel: ObservableObject{
         
         for (index, recipe) in recipe_id.enumerated(){
             if (id == recipe){
+                recipe_id = Array(Set(recipe_id))
                 recipe_id.remove(at: index)
+                recipe_id = Array(Set(recipe_id))
             }
         }
         
